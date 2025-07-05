2127d8814eac0c184340dbb9e29d9a938914c7e7
Parth Dhat <parthdhat@gmail.com>
2025-07-05 10:34:54 +0530

    Connect with Railway using env vars

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
-
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,     
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: 3306
 });
 
-
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL Database!');
+  if (err) {
+    console.error('Database connection failed:', err);
+    return;
+  }
+  console.log('Connected to MySQL Database!');
 });
 
 module.exports = connection;
